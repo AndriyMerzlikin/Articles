db977cee49addcee58eb474c1a02bec2bc33a9cd
AndriyMerzlikin <lastheroofme@gmail.com>
2024-08-26 09:45:35 +0300

    first commit frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 import Container from "@mui/material/Container";
 
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
 
 function App() {
+  console.log("hello");
   return (
     <>
       <Header />
